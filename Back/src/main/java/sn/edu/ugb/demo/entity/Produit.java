9ed8e880184e8e9092d2eb43f8ad89f8b9276e23
Zeydia <douseydiallo2002@gmail.com>
2026-03-03 15:55:47 +0000

    init Back

---
+++ Back/src/main/java/sn/edu/ugb/demo/entity/Produit.java
@@ -0,0 +1,34 @@
+package sn.edu.ugb.demo.entity;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "produits")
+public class Produit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nom;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal prix;
+
+    @Column(nullable = false)
+    private Integer quantite;
+
+    @Column
+    private String description;
+
+    
+
+    
+}
+
